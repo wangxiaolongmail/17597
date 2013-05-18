0ceceef389806fffa17af4dc935cdaf77fda509c
wangxiaolong <wangxiaolongmail@163.com>
2013-05-18 21:47:36 +0800

    wxl add supervisor 2013-05-18

--- _stop.sh
+++ _stop.sh
@@ -5,6 +5,8 @@ source _common.sh
 
 currentPath=`pwd`
 
-ps -ef | grep ${currentPath}/_start.js | grep -v grep | cut -c 9-15 | xargs kill -s 9
+#ps -ef | grep ${currentPath}/_start.js | grep -v grep | cut -c 9-15 | xargs kill -s 9
+
+ps -ef | grep _start.js | grep -v grep | cut -c 9-15 | xargs kill -s 9
 
 echo "stop server"
