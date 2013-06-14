2c689b308179e8c01b44bc0fc1ac4b7a7fca92fd
wangxiaolong <wangxiaolongmail@163.com>
2013-06-14 18:02:57 +0800

    update

--- _bin/store/api/_push_log.js
+++ _bin/store/api/_push_log.js
@@ -3,6 +3,7 @@ db.system.js.save({_id:"_push_log",value:function (params) {
 	var op={};
 	op[C.VALUE]=params;
 	op[C.INSERT_TIME]=(new Date()).getTime();
+	op[C.TYPE]=C.REQUEST;
 	var name="log_"+_get_date();
 	db[name].insert(op);
 }})
