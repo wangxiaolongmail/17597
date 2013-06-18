bcdb34e5c6c5b049984e26faa4daa127fb9605e5
wangxiaolong <wangxiaolongmail@163.com>
2013-06-18 21:19:09 +0800

    fix

--- _bin/store/admin/admin_Save.js
+++ _bin/store/admin/admin_Save.js
@@ -8,6 +8,7 @@ db.system.js.save({_id:"admin_Save",value:function (params) {
 		result[C.MODULE_LIST]=sess[C.MODULE_LIST];
 		result[C.R_MODULE_LIST]=sess[C.R_MODULE_LIST];
 		result[C.CURRENT_MODULE]=sess[C.CURRENT_MODULE];
+		db[params.TABLE_NAME].insert(params.INSERT_OBJ);
 		return result;
 	}else{
 		return sess;
