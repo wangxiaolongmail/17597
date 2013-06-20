e491569b0b3059a566bcb58f462f599e4da5ff8e
wangxiaolong <wangxiaolongmail@163.com>
2013-06-20 23:50:38 +0800

    fix

--- _bin/script/com/easysoft/service/Index.js
+++ _bin/script/com/easysoft/service/Index.js
@@ -61,30 +61,30 @@ dojo.declare( "com.easysoft.service.Index" , "com.easysoft.service.Tempalte" , {
 		var a=[],o={},I18N=dojo.I18N,C=dojo.C;
 		this.cur_obj={};
 		var mlist=this.getMenuList(data);
-		var list=mlist[dojo.C.LEFT];
-		var len = list.length;
 		a.push("<ul class='nav'>");
-		dojo.each(list,function(k,v,i){
-				var o=v;
-				if(o[C.IS_MENU]){
-					if(data[C.CURRENT_MODULE]==o[C.MODULE_NAME]){
-					  this.cur_obj=o;
-					  a.push("<li class='active'>");
-					  a.push("<a href='#'>"+I18N[o[C.MODULE_NAME]]+"</a>");
-					  a.push("</li>");
-					}else{
-					  a.push("<li>");
-					  a.push("<a href='"+o[C.MODULE_URL]+"'>"+I18N[o[C.MODULE_NAME]]+"</a>");
-					  a.push("</li>");
-					}
+		dojo.each(mlist[C.LEFT],function(k,v,i){
+			var o=v;
+			if(o[C.IS_MENU]){
+				if(data[C.CURRENT_MODULE]==o[C.MODULE_NAME]){
+					this.cur_obj=o;
+					a.push("<li class='active'>");
+					a.push("<a href='#'>"+I18N[o[C.MODULE_NAME]]+"</a>");
+					a.push("</li>");
+				}else{
+					a.push("<li>");
+					a.push("<a href='"+o[C.MODULE_URL]+"'>"+I18N[o[C.MODULE_NAME]]+"</a>");
+					a.push("</li>");
 				}
+			}
 		},this);
 		a.push("</ul>");
-		var o=mlist[C.RIGHT][0];
 		a.push("<ul class='nav pull-right'>");
-				a.push("<li>");
-			a.push("<a target='_blank' href='"+o[C.MODULE_URL]+"'>"+I18N[o[C.MODULE_NAME]]+"</a>");
+		dojo.each(mlist[C.RIGHT],function(k,v,i){
+			a.push("<li>");
+			a.push("<a target='_blank' href='"+v[C.MODULE_URL]+"'>"+I18N[v[C.MODULE_NAME]]+"</a>");
 			a.push("</li>");
+			
+		});
 		a.push("</ul>");
 		return a.join("");
 	}
