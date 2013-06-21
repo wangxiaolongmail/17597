a49cb39170aeaa320599da15d66e08f874fe1675
wangxiaolong <wangxiaolongmail@163.com>
2013-06-21 16:56:26 +0800

    fix

--- _bin/store/api/init.js
+++ _bin/store/api/init.js
@@ -1,38 +1,46 @@
 db.system.js.save({_id:"init",value:function () {
 	var pub=public();
 	var C=constant();
-	favorite_catlist=db.favorite_type.find({},{article_title:true,category:true}).toArray();
 	
-	var favorite_type={};
-	var a=favorite_catlist;
-	for(var i=0;i<a.length;i++){
-		favorite_type[a[i].category]=a[i].article_title;
-	}
-        var result={
+    var result={
 		C:C,
 		metadata:_get_metadata()
-        };
+    };
+		
+	var a=db.favorite_type.find({},{article_title:true,category:true}).toArray();
+	var favorite_type={};
+	_each(a,function(k,v,i){
+		favorite_type[v.category]=v.article_title;
+	});
+	result[C.FAVORITE_TYPE]=favorite_type;
+	var dict={};
+	var favorite_type={};
+	_each(a,function(k,v,i){
+		favorite_type[v._id.valueOf()]=v.article_title;
+	});
+	dict[C.FAVORITE_TYPE]=favorite_type;
+	
 	var I18N={};
 	var a=db.i18n.find().toArray()
-	for(var i=0;i<a.length;i++){
-		var o=a[i];
-		I18N[o._id]=o.cn;
-	}
+	_each(a,function(k,v,i){
+		I18N[v._id]=v.cn;
+	});
 	result[C.I18N]=I18N;
-	result[C.FAVORITE_TYPE]=favorite_type;
+	
 	var dynamicServletMapping=[];
-	var a=db.module.find().toArray()
-	for(var i=0;i<a.length;i++){
-		var o=a[i];
+	var a=db.module.find().toArray();
+	_each(a,function(k,v,i){
+		var o=v;
 		var op={};
 		op[C.SERVLET_CLASS]=o[C.SERVLET_CLASS];
 		op[C.PATH_NAME]=o[C.MODULE_URL];
 		op[C.METHOD]=o[C.METHOD];
 		dynamicServletMapping.push(op);	
-	}
+	});
 	result["dynamicServletMapping"]=dynamicServletMapping;
 	result[C.ROLE]=_get_role();
 	result[C._ID]=C.APPLICATION;
+	result[C.DICT]=dict;
 	_push_mdata(result)
 	return result;
 }})
