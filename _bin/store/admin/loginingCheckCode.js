5f2bfbec1136c5b6e9d6a2e95c8fe9dfec328068
wangxiaolong <wangxiaolongmail@163.com>
2013-06-24 14:52:45 +0800

    fix init.js

--- _bin/store/admin/loginingCheckCode.js
+++ _bin/store/admin/loginingCheckCode.js
@@ -2,6 +2,8 @@ db.system.js.save({_id:"loginingCheckCode",value:function (params) {
 	var C=constant();
 	var op={};
 	op[C.MODULE_URL]=_get_url(C.EASYSOFT+C.LOGINING);
+	var obj=_get_mdata(params[C.MID]);
+	_mixin(op,obj);
 	op.ok=true;
 	return op;
 }})
--- _bin/store/api/init.js
+++ _bin/store/api/init.js
@@ -33,6 +33,11 @@ db.system.js.save({_id:"init",value:function () {
 		dynamicServletMapping.push(op);	
 	});
 	result["dynamicServletMapping"]=dynamicServletMapping;
+	var map={};
+	_each(a,function(k,v,i){
+		map[v[C.MODULE_NAME]]=v[C.MODULE_URL];
+	});
+	result[C.URL]=map;
 	result[C.ROLE]=_get_role();
 	result[C._ID]=C.APPLICATION;
 	result[C.DICT]=dict;
