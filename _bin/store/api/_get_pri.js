290fa6d69c89ee36092aef4f708d8816548067f5
wangxiaolong <wangxiaolongmail@163.com>
2013-06-27 17:45:35 +0800

    fix

--- _bin/store/api/_get_pri.js
+++ _bin/store/api/_get_pri.js
@@ -3,7 +3,7 @@ db.system.js.save({_id:"_get_pri",value:function (tn) {
 	var a=db[tn].find().sort({PRI:-1}).limit(1).toArray();
    	var wrap={};
 	if(a.length>0){
-		wrap[tn]=a[0][C.PRI]+1;
+		wrap[tn]=a[0][C.PRI];
 	}else{
 		wrap[tn]=1;
 	}
