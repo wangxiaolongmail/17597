4429effa6f2440b2a368ce1eba0afdd97e91389e
wangxiaolong <wangxiaolongmail@163.com>
2013-06-29 20:00:25 +0800

    fix2

--- _bin/script/com/easysoft/service/admin/favorite/Insert.js
+++ _bin/script/com/easysoft/service/admin/favorite/Insert.js
@@ -15,43 +15,44 @@
 dojo.provide("com.easysoft.service.admin.favorite.Insert");
 dojo.declare( "com.easysoft.service.admin.favorite.Insert" , "com.easysoft.service.admin.favorite.Add" , {
         postCreate:function(){
-			var a=[],o={},op={};
-			var op=this.getsbo();
-			var obj={};
-			var a=this.get_schema_list();
-			dojo.each(a,function(k,v,i){
-				var field=v[C.FIELD];
-				var type=v[C.TYPE];
-				if(field!=C._ID){
-					if(type==C.PRI){
-						obj[field]= parseInt(this.queryForm[field]);
-					}else if(type==C.URL){
-						var s= this.queryForm[field];
-						if(s.indexOf("http://")<0){
-							s="http://"+s;
-						}
-						obj[field]= s;
-					}else{
-						obj[field]= this.queryForm[field];
+		var a=[],o={},op={};
+		var op=this.getsbo();
+		var obj={};
+		var a=this.get_schema_list();
+		dojo.each(a,function(k,v,i){
+			var field=v[C.FIELD];
+			var type=v[C.TYPE];
+			val=this.queryForm[field];
+			if(typeof val != "undefined"){
+				if(type==C.PRI){
+					obj[field]= parseInt(val);
+				}else if(type==C.URL){
+					var s= val ;
+					if(s.indexOf("http://")<0){
+						s="http://"+s;
 					}
+					obj[field]= s;
+				}else{
+					obj[field]= val;
 				}
-			},this);
-			op[C.INSERT+C.OBJECT] =obj;
-			op[C.TABLE_NAME] =this.table_name;
-			op[C.STORED_METHOD] ='admin_Insert';
-			this.exec(op);
-        },
-		postDraw:function(data){
-			if(data[C.IS_DICT]){
-				dojo.mixin(DICT,dojo.clone(data[C.DICT]));
 			}
-			var a=[],o={};
-			var $ = this.getDom();
-			$(".nav-collapse").html(this.drawMainMenu(data));
-			
-			$("#apbody").html("hello");
-			
-			var s=$.html();
-			return s;
+		},this);
+		op[C.INSERT+C.OBJECT] =obj;
+		op[C.TABLE_NAME] =this.table_name;
+		op[C.STORED_METHOD] ='admin_Insert';
+		this.exec(op);
+        },
+	postDraw:function(data){
+		if(data[C.IS_DICT]){
+			dojo.mixin(DICT,dojo.clone(data[C.DICT]));
 		}
+		var a=[],o={};
+		var $ = this.getDom();
+		$(".nav-collapse").html(this.drawMainMenu(data));
+			
+		$("#apbody").html("hello");
+			
+		var s=$.html();
+		return s;
+	}
 });
