d3a423c3610e13b3ffee13467e9bd9aeb577afae
wangxiaolong <wangxiaolongmail@163.com>
2013-07-09 16:00:05 +0800

    fix

--- _bin/script/com/easysoft/service/admin/profile/Introduce.js
+++ _bin/script/com/easysoft/service/admin/profile/Introduce.js
@@ -16,13 +16,33 @@ dojo.provide("com.easysoft.service.admin.profile.Introduce");
 dojo.declare( "com.easysoft.service.admin.profile.Introduce" , "com.easysoft.service.admin.Start" , {
 	table_name:C.USER,
 	postCreate:function(){
-		var a=[],o={},op={};
 		var op=this.getsbo();
 		op[C.TABLE_NAME] =this.table_name;
 		op[C.STORED_METHOD] =C.ADMIN;
 		this.exec(op);
 	},
-	postDraw:function($,data){
-		return "";
+	_define_schema:function(k,v,i){
+		if( v[C.FIELD]===C.USER_NAME ){
+			v[C.FORMAT]=function(k,v,i,tn){
+				var a=[];
+				a.push("<div class=\"control-group\">");
+				a.push("<label>");
+				a.push(I18N[v[C.FIELD]]);
+				a.push("</label>");
+				a.push("<input type=\"text\" name=\""+v[C.FIELD]+"\" class=\"span3\" style=\"height:30px\">");
+				a.push("</div>");
+				return a.join("\n");
+			}
+		 }
+	},
+	postDrawEx:function($,data){
+		var a=[];	
+		var metadata=this.define_schema();
+		dojo.each(metadata,function(k,v,i){
+			if(v[C.FORMAT]){
+				a.push(v[C.FORMAT].call(this,k,v,i,this.table_name,data[C.MID]));
+			}			
+		},this);
+		$("#apBody").html(a.join("\n"));
 	}
 });
